963b60333054e9f82da5277d830ff373032f1ea5
IshaSangani <51543957+IshaSangani@users.noreply.github.com>
2021-01-14 11:31:42 -0800

    updated styles

--- personal-assistant/src/Styles.js
+++ personal-assistant/src/Styles.js
@@ -1,1 +1,40 @@
 import 'styled';
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: absolute;
+  bottom: 50%;
+  left: 20%;
+  right: 20%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 4px;
+  background-color: lightblue;
+  font-family: 'Times New Roman';
+  transition: 0.5s ease;
+  &:hover {
+    box-shadow: 10px 10px 5px lightgrey;
+  }
+`
+
+export const Text = styled.h2`
+  letter-spacing: 6px;
+  color: white;
+  text-align: center;
+  font: 'Times New Roman';
+  padding: 4px;
+  font-size: 20px;
+`
+
+export const Other = styled.div`
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font: 'Times New Roman';
+`
+
+export const Strike = styled.td`
+  text-decoration: line-through;
+`
